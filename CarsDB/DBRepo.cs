2160080c9f452f52995eccb1bcfc5fa7b0fa4b7d
Anthony <tejant96@gmail.com>
2020-11-12 11:48:47 -0500

    one dbrepo method added

---
+++ CarsDB/DBRepo.cs
@@ -0,0 +1,27 @@
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using CarsDB.Interfaces;
+using CarsDB.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsDB
+{
+    public class DBRepo : ICarRepoMethods
+    {   
+        private CarsContext context = new CarsContext();
+        
+        public DBRepo(CarsContext context){
+
+            this.context = context;
+
+        }
+
+        public List<Cars> GetAllCars(){
+
+            context.Cars.Select(x=> x).ToList();
+        }
+    }
+}
