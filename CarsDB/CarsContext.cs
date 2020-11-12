88f9bb1407f3094b91706a5b89d728a016b44389
Anthony <tejant96@gmail.com>
2020-11-12 10:50:43 -0500

    context added

---
+++ CarsDB/CarsContext.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using CarsLib;
+
+namespace CarsDB
+{
+    public class CarsContext : DBContext
+    {
+        public DbSet<Brands> Brands {get; set;}
+        public DbSet<Cars> Cars {get; set;}
+        public DbSet<Customer> Customer {get; set;}
+        public DbSet<Inventory> Inventories {get; set;}
+        public DbSet<Location> Locations {get; set;}
+        public DbSet<Order> Orders {get; set;}
+        public DbSet<OrderItem> OrderItems {get; set;}
+        public DbSet<Years> Years {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+
+            //if no connection to database configure one 
+            if(!optionsBuilder.IsConfigured)
+            {
+                //look for json file to configure 
+                var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+                var connectionString = configuration.GetConnectionString("CarsDB");
+
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+    }
+
+}
