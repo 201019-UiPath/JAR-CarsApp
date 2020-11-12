8c25c517c07e6263a711f0c9298012292bcbbc22
Rachel Hufnagel <rachelhufnagel_2020@yahoo.com>
2020-11-12 13:18:18 -0600

    dbrepo

--- CarsDB/DBRepo.cs
+++ CarsDB/DBRepo.cs
@@ -19,10 +19,46 @@ namespace CarsDB
 
         }
 
-        public List<Cars> GetAllCars(){
+        public void AddCar(Cars car)
+        {
+            context.Cars.Add(car);
+            context.SaveChanges();
+        }
 
-            List<Cars> car = context.Cars.Select(x=> x).ToList();
+        public void DeleteCar(Cars car)
+        {
+            context.Cars.Remove(car);
+            context.SaveChanges();
+        }
+
+        public List<Cars> GetAllCars()
+        {
+            List<Cars> car = context.Cars.Select(c => c).ToList();
             return car;
+        }
+
+        public Cars GetCarByBrand(int brandId)
+        {
+            Cars car = context.Cars.Single(c => c.brandId == brandId);
+            return car;
+        }
+
+        public Cars GetCarByCarId(int carId)
+        {
+            Cars car = context.Cars.Single(c => c.Id == carId);
+            return car;
+        }
+
+        public Cars GetCarByYear(int yearId)
+        {
+            Cars car = context.Cars.Single(c => c.yearId == yearId);
+            return car;
+        }
+
+        public void UpdateCare(Cars car)
+        {
+            context.Cars.Update(car);
+            context.SaveChanges();
         }
     }
 }
--- CarsDB/Interfaces/ICarRepoMethods.cs
+++ CarsDB/Interfaces/ICarRepoMethods.cs
@@ -5,5 +5,13 @@ namespace CarsDB.Interfaces
     public interface ICarRepoMethods
     {
          List<Cars> GetAllCars();
+         void AddCar(Cars car); 
+         void UpdateCare(Cars car);
+         void DeleteCar(Cars car);
+         Cars GetCarByCarId(int carId);
+         Cars GetCarByYear(int yearId);
+         Cars GetCarByBrand(int brandId);
     }
+
+
 }
